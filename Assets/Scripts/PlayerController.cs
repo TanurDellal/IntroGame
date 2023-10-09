e8cad1e4246fe453066c0737bcee7065c6aa62c7
Tanur Dellal <ec21784@qmul.ac.uk>
2023-10-09 14:51:43 +0100

    playerController updated, UI and count

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private int count;
+    private int numPickups;
+    public Vector2 moveValue;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI winText;
+
+    private void Start()
+    {
+        count = 0;
+        winText.text = "";
+        SetCountText();
+
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveValue = value.Get<Vector2>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(moveValue.x, 0.0f, moveValue.y);
+        
+        GetComponent<Rigidbody>().AddForce(movement * speed * Time.fixedDeltaTime);
+     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "PickUp")
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    private void SetCountText()
+    {
+        scoreText.text = "Score: " + count.ToString();
+
+        if (count >= numPickups)
+        {
+            winText.text = "You win!";
+        }
+    }
+}
